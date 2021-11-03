084ec983adbdbee6989c390d8a4611796e6c85fc
Katie Kimura <katiekimura2022@gmail.com>
2021-11-02 22:03:42 -1000

    Organized naming conventions

--- MARSproject/MARSapp/views.py
+++ MARSproject/MARSapp/views.py
@@ -3,23 +3,21 @@ from .forms import formdata
 from .models import formdata
 # Create your views here.
 
-def userlanding(request):
-    return render(request, 'userlanding.html', )
-
-def hmarlanding(request):
-    return render(request, 'hmartest.html')
-
-#def userlanding(request):
-    #return render(request, 'userlanding.html')
+def home(request):
+    return render(request, 'home.html', )
+'''
+def hmar(request):
+    return render(request, 'hmar.html')
+'''
 
 def emergency(request):
-    return render(request, 'emergencyland.html')
+    return render(request, 'emergency.html')
 
-def loginpage(request):
-    return render(request, 'loginpage.html')
+def login(request):
+    return render(request, 'login.html')
 
-def basicpage(request):
-    return render(request, 'sightingform.html')
+def sighting(request):
+    return render(request, 'sighting.html')
 
 
 
