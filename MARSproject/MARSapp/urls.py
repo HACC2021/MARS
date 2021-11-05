5a1b4e1bee0bd7b3f9a414d01326920007481557
aplus2 <Cherrybombman123@gmail.com>
2021-11-04 19:23:19 -1000

    11/4 updated views

--- MARSproject/MARSapp/urls.py
+++ MARSproject/MARSapp/urls.py
@@ -4,10 +4,14 @@ from django.urls import include
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #path('hmar/', views.hmar, name='hmar'),
+
     path('emergency/', views.emergency, name = 'emergency'),
     path("login/", views.login, name = 'login'),
     path('sighting/', views.sighting, name = 'sighting'),
+    path('hmar/', views.hmar, name='hmar'),
+    path('editform/', views.editform, name = 'editform'),
+    path('viewspecificreport/', views.viewspecificreport, name = 'viewspecificreport'),
+    path('submitform/', views.submitform, name = 'submitform')
 
 
 
--- MARSproject/MARSapp/views.py
+++ MARSproject/MARSapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
-from .forms import formdata
-from .models import formdata
+from pymongo import MongoClient
 # Create your views here.
+
+client = MongoClient("mongodb+srv://Payton_Ikeda:mililanihs@cluster0.lrsp5.mongodb.net/hacc_sample_database?retryWrites=true&w=majority")
+database = client["hacc_sample_database"]
+collection = database["hacc_sample_collection"]
+
 
 def home(request):
     return render(request, 'home.html', )
@@ -22,8 +23,52 @@ def login(request):
 def sighting(request):
     return render(request, 'sighting.html')
 
+def submitform(request):
+    submit = (request.POST.get('submit'))
+    submission_dict = {}
+    if (submit == "submitted"):
+        fname = (request.POST.get('fname'))
+        lname = (request.POST.get('lname'))
+        pnumber = (request.POST.get('pnumber'))
+        datetime = (request.POST.get('datetime'))
+        animalcharacteristics = (request.POST.get('AnimalCharacteristics'))
+        island = (request.POST.get('Island'))
+        behavior = (request.POST.get('behavior'))
+        interaction = (request.POST.get('interaction1'))
+        humans = (request.POST.get('humans'))
+        filename = (request.POST.get('filename'))
+        latitude = (request.POST.get('latitude'))
+        print(fname)
+        print(type((fname)))
+        submission_dict["first name"].append(fname)
+        print(submission_dict)
+        submission_dict["first name":fname]
+        submission_dict["last name":lname]
+        submission_dict["phone number":pnumber]
+        submission_dict["datetime":datetime]
+        submission_dict["animal characteristics":animalcharacteristics]
+        submission_dict["island":island]
+        submission_dict["behavior":behavior]
+        submission_dict["interaction":interaction]
+        submission_dict["humans":humans]
+        submission_dict["filename":filename]
+        print(submission_dict)
+
+
+    return render(request, 'thankyou.html')
 
 
 
 
+def hmar(request):
+    context = {
+    "all_data" : collection.find({})
+    }
+    print(context)
+    return render(request, 'hmarlanding.html',context)
 
+def editform(request):
+    return render(request, 'editform.html')
+
+def viewspecificreport(request):
+    return render(request, 'viewspecificreport.html')
